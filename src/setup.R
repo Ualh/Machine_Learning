92721539ade5ca4bb0c3b22ef1842a0de8a4bc27
Urs <urs.hurni@unil.ch>
2024-04-29 12:07:55 +0200

    website update

--- src/setup.R
+++ src/setup.R
@@ -50,31 +50,31 @@ purrr::walk(packages, function(pkg) {
 ######################################################
 # You can use this if you wanted to change the rendered output
 
-# general options
-# options(
-#   digits = 3,
-#   str = strOptions(strict.width = "cut"),
-#   width = 69,
-#   tibble.width = 69,
-#   cli.unicode = FALSE
-# )
+#general options
+options(
+  digits = 3,
+  str = strOptions(strict.width = "cut"),
+  width = 69,
+  tibble.width = 69,
+  cli.unicode = FALSE
+)
 
 # ggplot options for the report
 theme_set(theme_light())
 
-# knitr options 
-# opts_chunk$set(
-#   comment = "#>",
-#   collapse = TRUE,
-#   # cache = TRUE,
-#   fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
-#   dpi = 300,
-#   out.width = "70%",
-#   fig.align = "center",
-#   fig.width = 6,
-#   fig.asp = 0.618,
-#   fig.show = "hold",
-#   message = FALSE,
-#   echo = FALSE
-# )
+#knitr options
+opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  # cache = TRUE,
+  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+  dpi = 300,
+  out.width = "70%",
+  fig.align = "center",
+  fig.width = 6,
+  fig.asp = 0.618,
+  fig.show = "hold",
+  message = FALSE,
+  echo = FALSE
+)
 
